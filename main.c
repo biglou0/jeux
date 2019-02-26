9db42af739928a79767c5bad0f3ae5c74d10180d
ihebferiani <42569011+ihebferiani@users.noreply.github.com>
2019-02-27 00:59:56 +0100

    iheb

--- main.c
+++ main.c
@@ -20,33 +20,52 @@ SDL_Surface* Hset=NULL;
 SDL_Surface* Hleave=NULL;
 SDL_Surface* tmp=NULL;
 SDL_Event event={0};
+SDL_Event eevent={0};
+SDL_Surface* flowb=NULL;
+SDL_Surface* menuu=NULL;
+SDL_Rect menub={200,100,800,600};
+SDL_Surface* backb=NULL;
+SDL_Rect backr={650,500,230,58};
+SDL_Surface* on=NULL;
+SDL_Surface* off=NULL;
+SDL_Rect setm={750,218,100,50};
+SDL_Rect sets={750,340,100,50};
+SDL_Rect setshadow={750,448,100,50};
 int main()
 {
+int son=1;
+int music=1;
+int shadow=1;
 int run=0;
 int menu=1;
 int now=0;
 int ex=0;
-int pfps=5;
+int pfps=2;
 int dt=0;
 int etat=4;
-int settingg;
+int game=1;
+int settingg=0;
 float pfpss=0;
 SDL_Init(SDL_INIT_VIDEO);
 const SDL_VideoInfo *pinfo=SDL_GetVideoInfo();
 int bpp=pinfo->vfmt->BitsPerPixel;
 //images
-backg=IMG_Load("ress/def.bmp");
+backg=SDL_LoadBMP("ress/def.bmp");
 SDL_GetClipRect(backg,&backgrect);
 play=IMG_Load("ress/aa.png");
 setting=IMG_Load("ress/set.png");
 leave=IMG_Load("ress/leave.png");
 Hplay=IMG_Load("ress/aasel.png");
-SDL_SetColorKey( Hplay , SDL_SRCCOLORKEY, SDL_MapRGB( Hplay->format, 0, 0, 0) );
-
 Hset=IMG_Load("ress/setsel.png");
 Hleave=IMG_Load("ress/leavesel.png");
+menuu=IMG_Load("ress/menu.png");
+backb=IMG_Load("ress/back.png");
+flowb=SDL_LoadBMP("ress/flouu.bmp");
+on=IMG_Load("ress/on.png");
+off=IMG_Load("ress/off.png");
 screen=SDL_SetVideoMode(LARGEUR,HAUTEUR,bpp,SDL_HWSURFACE);
 SDL_WM_SetCaption("Menu",NULL);
+while (game){
 while(menu)
 {
 
@@ -74,13 +93,15 @@ SDL_PollEvent(&event);
 switch(event.type)
 {case SDL_QUIT:
 {
-    menu=0;
+menu=0;
+    game=0;
   }
 break;
 case SDL_KEYDOWN:
  if(event.key.keysym.sym==SDLK_DOWN)
 
   {
+
 etat--;
 SDL_Delay(100);
   }
@@ -101,6 +122,7 @@ if(etat==3){run=1,menu=0;}
 }
 break;
 case SDL_MOUSEMOTION:
+
 if(94<event.motion.x&&event.motion.x<284)
 {if(448<event.motion.y&&event.motion.y<506)
 {
@@ -127,7 +149,9 @@ etat=1;
 break;
 case SDL_MOUSEBUTTONDOWN:
 if( event.button.button == SDL_BUTTON_LEFT )
-		{if(94<event.button.x&&event.button.x<284)
+		{
+
+if(94<event.button.x&&event.button.x<284)
 {if(596<event.button.y&&event.button.y<652)
 {
 menu=0;
@@ -151,12 +175,72 @@ ex=now;
 }else
  SDL_Delay(pfps-dt);
 }
+
+while(settingg){
+now=SDL_GetTicks();
+dt=now-ex;
+if(dt>=pfps)
+{
+SDL_BlitSurface(flowb,NULL,screen,&backgrect);
+SDL_BlitSurface(menuu,NULL,screen,&menub);
+if(music==1){
+SDL_BlitSurface(on,NULL,screen,&setm);}
+if(music==0){
+SDL_BlitSurface(off,NULL,screen,&setm);}
+if(son==1){
+SDL_BlitSurface(on,NULL,screen,&sets);}
+if(son==0){
+SDL_BlitSurface(off,NULL,screen,&sets);}
+if(shadow==1){
+SDL_BlitSurface(on,NULL,screen,&setshadow);}
+if(shadow==0){
+SDL_BlitSurface(off,NULL,screen,&setshadow);}
+SDL_PollEvent(&event);
+switch(event.type)
+{case SDL_QUIT:
+settingg=0;
+game=0;
+break;
+case SDL_MOUSEBUTTONDOWN:
+if( event.button.button == SDL_BUTTON_LEFT )
+		{
+
+if(750<event.button.x&&event.button.x<850)
+{if(218<event.button.y&&event.button.y<258)
+{
+music++;
+SDL_Delay(200);
+if(music==2){music=0;}
+}else if(340<event.button.y&&event.button.y<383)
+{
+son++;
+SDL_Delay(200);
+if(son==2){son=0;}
+}else if(448<event.button.y&&event.button.y<488)
+{
+shadow++;
+SDL_Delay(200);
+if(shadow==2){shadow=0;}
+}
+}
+}
+break;
+}
+SDL_Flip(screen);
+}else
+ SDL_Delay(pfps-dt);
+}
+
+}
 SDL_FreeSurface(play);
 SDL_FreeSurface(setting);
 SDL_FreeSurface(leave);
 SDL_FreeSurface(Hplay);
 SDL_FreeSurface(Hset);
 SDL_FreeSurface(Hleave);
+
+
+
 SDL_Quit();
 return 0;
 }
