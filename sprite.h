62bcf8b2b063f798532c203cde15ad81ee8b8b8e
amina112 <47774609+amina112@users.noreply.github.com>
2019-04-07 05:39:16 -0700

    Add files via upload

---
+++ sprite.h
@@ -0,0 +1,22 @@
+#ifndef OBJET_H_INCLUDED
+
+#define OBJET_H_INCLUDED
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+/* Size of the sprite; this is a square */
+#define SPRITE_SIZE   32
+/* Size of the grass texture picture */
+#define GRASS_SIZE    32
+#include <SDL.h>
+
+
+
+
+struct sprite{
+ SDL_Rect spritePosition;
+
+};
+
+#endif
+
+
