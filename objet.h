85ab49df99b5007dbc1afc006634cde6b054aaad
amina112 <47774609+amina112@users.noreply.github.com>
2019-04-10 12:31:15 -0700

    Add files via upload

---
+++ objet.h
@@ -0,0 +1,32 @@
+#ifndef OBJET_H_INCLUDED
+
+#define OBJET_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <string.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
+
+typedef struct {
+
+SDL_Surface* image1;
+SDL_Surface* image2;
+SDL_Surface* image3;
+SDL_Surface* image4;
+SDL_Surface* image5;
+
+SDL_Rect position1,position2,position3,position4,position5;
+
+}objet;
+void init_objet(objet *o,SDL_Surface *ecran);
+
+void afficherobjet(objet *o,SDL_Surface *ecran);
+
+
+#endif
+
+
